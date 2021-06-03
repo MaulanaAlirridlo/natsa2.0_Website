0c7651cd6bec0a3be3e3024cea41a0a068d27185
irhasalif <irhasalif@gmail.com>
2021-06-03 19:51:13 +0700

    pesan validasi di vestige dan fix typo di irrigation

--- resources/views/admin/irrigations/irrigationPut.blade.php
+++ resources/views/admin/irrigations/irrigationPut.blade.php
@@ -17,9 +17,9 @@
                         <span class="text-gray-700 dark:text-gray-400">Irigasi</span>
                         <input
                             class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-                            placeholder="Jenis Irigasi" name="irrigation" value="{{ $irrigation->irrigation }}" />
+                            placeholder="Jenis Irigasi" name="irrigation" required maxlength="100" value="{{ $irrigation->irrigation }}" />
                     </label>
-                    @error('kabupaten')
+                    @error('irrigation')
                     <span class="text-xs text-red-600 dark:text-red-400">
                         {{ $message }}
                     </span>
