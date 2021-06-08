8b68c447fd1b234fae870a36d5aa15a0012e67b5
irhasalif <irhasalif@gmail.com>
2021-06-08 13:25:40 +0700

    ubah fisibilitas API
    untuk sekarang sanctum cuma ada di riceField store, delete, dan put

--- routes/api.php
+++ routes/api.php
@@ -24,80 +24,78 @@ use App\Http\Controllers\API\VerificationController;
 //     return $request->user();
 // });
 
+//Regions
+Route::prefix('regions')->group(function () {
+
+    Route::get('/', [RegionController::class, 'index']);
+    Route::get('/{id}', [RegionController::class, 'show']);
+    Route::get('/search/{search}', [RegionController::class, 'search']);
+
+});
+
+//Regions
+Route::prefix('irrigations')->group(function () {
+
+    Route::get('/', [IrrigationController::class, 'index']);
+    Route::get('/{id}', [IrrigationController::class, 'show']);
+    Route::get('/search/{search}', [IrrigationController::class, 'search']);
+
+});
+
+//Vestiges
+Route::prefix('vestiges')->group(function () {
+
+    Route::get('/', [VestigeController::class, 'index']);
+    Route::get('/{id}', [VestigeController::class, 'show']);
+    Route::get('/search/{search}', [VestigeController::class, 'search']);
+
+});
+
+//Verifications
+Route::prefix('verifications')->group(function () {
+
+    Route::get('/', [VerificationController::class, 'index']);
+    Route::get('/{id}', [VerificationController::class, 'show']);
+    Route::get('/search/{search}', [VerificationController::class, 'search']);
+
+});
+
+//Social Media
+Route::prefix('socialMedias')->group(function () {
+
+    Route::get('/', [SocialMediaController::class, 'index']);
+    Route::get('/{id}', [SocialMediaController::class, 'show']);
+    Route::get('/search/{search}', [SocialMediaController::class, 'search']);
+
+});
+
+//Sawah / RiceField
+Route::prefix('riceFields')->group(function () {
+
+    Route::get('/', [RiceFieldController::class, 'index']);
+    Route::get('/{id}', [RiceFieldController::class, 'show']);
+    Route::get('/search/{search}', [RiceFieldController::class, 'search']);
+
+});
+
+//User
+Route::prefix('users')->group(function () {
+
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::get('/search/{search}', [UserController::class, 'search']);
+
+});
+
 Route::middleware('auth:sanctum')->group(function () {
-
-    //Regions
-    Route::prefix('regions')->group(function () {
-
-        Route::get('/', [RegionController::class, 'index']);
-        Route::get('/{id}', [RegionController::class, 'show']);
-        Route::get('/search/{search}', [RegionController::class, 'search']);
-
-    });
-
-    //Regions
-    Route::prefix('irrigations')->group(function () {
-
-        Route::get('/', [IrrigationController::class, 'index']);
-        Route::get('/{id}', [IrrigationController::class, 'show']);
-        Route::get('/search/{search}', [IrrigationController::class, 'search']);
-
-    });
-
-    //Vestiges
-    Route::prefix('vestiges')->group(function () {
-
-        Route::get('/', [VestigeController::class, 'index']);
-        Route::get('/{id}', [VestigeController::class, 'show']);
-        Route::get('/search/{search}', [VestigeController::class, 'search']);
-
-    });
-
-    //Verifications
-    Route::prefix('verifications')->group(function () {
-
-        Route::get('/', [VerificationController::class, 'index']);
-        Route::get('/{id}', [VerificationController::class, 'show']);
-        Route::get('/search/{search}', [VerificationController::class, 'search']);
-
-    });
-
-    //Social Media
-    Route::prefix('socialMedias')->group(function () {
-
-        Route::get('/', [SocialMediaController::class, 'index']);
-        Route::get('/{id}', [SocialMediaController::class, 'show']);
-        Route::get('/search/{search}', [SocialMediaController::class, 'search']);
-
-    });
 
     //Sawah / RiceField
     Route::prefix('riceFields')->group(function () {
 
-        Route::get('/', [RiceFieldController::class, 'index']);
-        Route::get('/{id}', [RiceFieldController::class, 'show']);
-        Route::get('/search/{search}', [RiceFieldController::class, 'search']);
         Route::post('/', [RiceFieldController::class, 'store']);
         Route::delete('/{id}', [RiceFieldController::class, 'destroy']);
         Route::put('/{id}', [RiceFieldController::class, 'update']);
 
     });
 
-    //Social Media
-    Route::prefix('socialMedias')->group(function () {
-
-        Route::get('/', [SocialMediaController::class, 'index']);
-        Route::get('/{id}', [SocialMediaController::class, 'show']);
-        Route::get('/search/{search}', [SocialMediaController::class, 'search']);
-
-    });
-
-    //User
-    Route::prefix('users')->group(function () {
-
-        Route::get('/', [UserController::class, 'index']);
-        Route::get('/{id}', [UserController::class, 'show']);
-        Route::get('/search/{search}', [UserController::class, 'search']);
-
-    });
 });
