3e50a2e5c869eb45192fb7f54ab536689d97f610
irhasalif <irhasalif@gmail.com>
2021-06-08 21:32:46 +0700

    fix avatar user #3

--- resources/views/admin/users/user.blade.php
+++ resources/views/admin/users/user.blade.php
@@ -92,7 +92,7 @@
                                 <div class="flex items-center text-sm">
                                     <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
                                         <img class="object-cover w-full h-full rounded-full"
-                                            src="{{ '/storage/'.$user->profile_photo_path }}" alt="" loading="lazy" />
+                                            src="{{ $user->profile_photo_url }}" alt="" loading="lazy" />
                                         <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true">
                                         </div>
                                     </div>
